aca470860e1e4b42bf60e77bab3041542cffee50
Leemainsw <yebom6476@gmail.com>
2026-01-22 16:41:46 +0900

    solve: 0122-배열

---
+++ solutions/yesul/0122/[문제 07] 방문 길이 ★★.js
@@ -0,0 +1,27 @@
+const coordinate = {
+    U: [0, 1],
+    D: [0, -1],
+    R: [1, 0],
+    L: [-1, 0],
+}
+
+function solution(dirs) {
+    let x = 0, y = 0;
+    const visited = new Set();
+
+    [...dirs].forEach(dir => {
+        const [dx, dy] = coordinate[dir];
+        // 좌표평면의 경계를 넘어가는 명령어는 무시
+        if (x + dx > 5 || x + dx < -5 || y + dy > 5 || y + dy < -5) return;
+
+        visited.add(`${x},${y},${x + dx},${y + dy}`);
+
+        x += dx;
+        y += dy;
+    })
+
+    return visited.size;
+}
+
+console.log(solution("ULURRDLLU"))
+console.log(solution("LULLLLLLU"))
